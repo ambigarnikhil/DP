f24502bd1f379555d95790b75301b1483e81734a
Nikhil <ambigarnikhil@gmail.com>
2021-10-30 23:52:16 +0530

    sum of n numbers program

---
+++ dp.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+/*
+int nsum(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    int dp[n+1];
+    dp[0]=0;
+    for(int i=1;i<=n;i++)
+    {
+        dp[i]=dp[i-1]+i;
+    }
+    return dp[n];
+}
+int main()
+{
+    cout<<nsum(5)<<endl;
+}
+*/
